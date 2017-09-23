e0473e5b6e2bf97f5538b72ad760012c541abf4a
kailinc <kailinc@bu.edu>
2017-09-22 23:57:53 -0400

    Restructure App
    Comfort: 5
    Clarity: 4
    
    - used Account Component to create another account for Savings
    - restructure App Component and Account Component so that the balances
      for each account is at a higher level.
    - this will allow for transfers to and from each account
    - Account Component calculates the new balance (withdraw, deposits) and
     lifts it up
    - react just renders it auto
    - referenced React Doct Lift State Up for help in lifting states
    https://facebook.github.io/react/docs/lifting-state-up.html
    
    Thoughts:
    I was very scared and uncomfortable with restructuring App Component to
    lift states but it was hard to implement this change at all. It took me
    a 40-60 mins to get it right. I am going to work on transferring funds
    from one account to another.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,36 @@ import logo from './ga.png';
 import Account from './Account';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      checkingsBalance: 100,
+      savingsBalance: 100
+    }
+    this.handleBalanceChange = this.handleBalanceChange.bind(this)
+    // this.handleCheckingsChange = this.handleCheckingsChange.bind(this)
+  }
+
+  // just updates the checkings balance
+  handleBalanceChange(accountInfo) {
+    if (accountInfo.type === 'Checking') {
+      this.setState({
+        checkingsBalance: accountInfo.balance
+      })
+    } else {
+      this.setState({
+        savingsBalance: accountInfo.balance
+      })
+    }
+  }
+
+  // just updates the savings balance
+  // handleSavingsChange(newBalance) {
+  //   this.setState({
+  //     savingsBalance: newBalance
+  //   })
+  // }
+
   render() {
     return (
       <div id="content">
@@ -11,7 +41,14 @@ class App extends Component {
           <div id="logo"><img src={logo} alt="General Assembly logo" /></div>
           <div id="title">Bank of GA</div>
         </div>
-        <Account name="Checking" />
+        <Account
+          name="Checking"
+          balance={this.state.checkingsBalance}
+          onAmountChange={this.handleBalanceChange} />
+        <Account
+          name="Savings"
+          balance={this.state.savingsBalance}
+          onAmountChange={this.handleBalanceChange}/>
         <div className="clear"></div>
       </div>
     );
